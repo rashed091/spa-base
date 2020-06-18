5ecfda59351d8d10927d61f1821646cc86b31ce9
mrasheduzzaman <rashed091nsu@gmail.com>
2020-06-18 21:35:48 +0600

    Remove experimental feature to own branch and cleanup base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import * as React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { IntlProvider } from 'react-intl';
-import { IntlProvider as RSIntlProvider } from 'rsuite';
-
-import enGB from 'rsuite/lib/IntlProvider/locales/en_GB';
-import locales from './locales';
-import Routes from './pages';
+import Routes from "./pages";
 
 const history = createBrowserHistory();
 
 class App extends React.PureComponent {
   render() {
     return (
-      <IntlProvider locale="en" messages={locales.en}>
-        <RSIntlProvider locale={enGB}>
-          <Router history={history}>
-            <Routes />
-          </Router>
-        </RSIntlProvider>
-      </IntlProvider>
+      <Router history={history}>
+        <Routes />
+      </Router>
     );
   }
 }
