ca965f6336baf05d60c85d7f24465dc787d45981
mrasheduzzaman <rashed091nsu@gmail.com>
2020-06-23 01:52:36 +0600

    Add login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./home";
-import Frame from "../components/Frame";
 import Login from "./login";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Frame} />;
-      <Route path="/login" component={Login} />;
+      <Route exact path="/login" component={Login} />;
     </Switch>
   );
 }
