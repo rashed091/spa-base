552a6629b22fd76b0028e1e3a9b35c62bafb634f
mrasheduzzaman <rashed091nsu@gmail.com>
2020-06-23 20:09:48 +0600

    Make landing page private

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-// import Home from "./home";
-// import Frame from "../components/Frame";
+import PrivateRoute from "../components/PrivateRoute";
 import Login from "./login";
 import LandingPage from "./landingPage";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={LandingPage} />;
+      {/* <Route exact path="/" component={LandingPage} />; */}
       <Route path="/login" component={Login} />;
+      <PrivateRoute path="/" component={LandingPage} />
     </Switch>
   );
 }
