dcc2483c8a15dc3faa81c28765c3a8f3a15e0a4a
mrasheduzzaman <rashed091nsu@gmail.com>
2020-06-23 17:16:28 +0600

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react"; // <-- updated
-import Login from "./pages/login";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { createBrowserHistory } from "history";
-// import Routes from "./pages";
-// const history = createBrowserHistory();
-// class App extends React.PureComponent {
-//   render() {
-//     return (
-//       <Router history={history}>
-//         <Routes />
-//       </Router>
-//     );
-//   }
-// }
+import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Routes from "./pages";
 
-function App() {
-  // const auth0 = useContext(Auth0Context); // <-- new
-
-  return <Login />;
+const history = createBrowserHistory();
+class App extends React.PureComponent {
+  render() {
+    return (
+      <Router history={history}>
+        <Routes />
+      </Router>
+    );
+  }
 }
+
 export default App;
